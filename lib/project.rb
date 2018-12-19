56d047b0ee66c7d8fbdde18dea16be796db08261
Tania Whiting <tanwhiting@aol.com>
2018-12-19 04:43:32 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title  
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    self.backers << backer  
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+end
+
